d8eca1942548e1ff9cd4b0d2728512970535a8b4
Belen Caporale <caporalebelen14@gmail.com>
2023-03-28 10:56:19 -0300

    astronauta

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -39,7 +39,7 @@ const Main = () => {
                             <p className='adquiriTextoMain'>TODAS INCLUYEN TRASLADO DE IDA Y VUELTA</p>
                             <Link to={"/Experiencias"} className='quienesBotonMain'>EXPERIENCIAS</Link>
                         </div>
-                        <div astronautaMain>
+                        <div className='astronautaMain' >
                             <img className='astroMain' src='./img/astronauta-04.png' />
                         </div>
                     </div>
@@ -54,7 +54,7 @@ const Main = () => {
 
                         <div className='orden'>
                             <h3 className='quienesSomos'>¿QUIÉNES SOMOS?</h3>
-                            <p className='quienesTexto'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                            <p className='quienesTexto'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
 
                             <Link to={"/Experiencias"} className='quienesBoton'>EXPERIENCIAS</Link>
                         </div>
