353b887f7ffbd82613a545aede09b751f2293a2d
Belen Caporale <caporalebelen14@gmail.com>
2023-03-28 17:23:22 -0300

    cambios

--- src/pages/Nosotros/Nosotros.js
+++ src/pages/Nosotros/Nosotros.js
@@ -7,7 +7,7 @@ const Nosotros = () => {
         <section className='quienes-section'>
             <div className='quienes-Padre'>
                 <div className='orden-section'>
-                    <h1 className='quienes-Somos'>SOBRE NOSOTROS</h1>
+                    <h1 className='quienes-Somos'>CONOCENOS MEJOR</h1>
                     <p className='quienes-Texto'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p>
 
                     <Link to={"/Experiencias"} className='quienesBoton'>
